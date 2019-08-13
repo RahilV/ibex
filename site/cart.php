fd49cc17576159ed95e27845429685971c22c746
vedant-shroff <vedant.shroff@somaiya.edu>
2019-08-13 14:56:04 +0530

    fixed cart.php

--- site/cart.php
+++ site/cart.php
@@ -41,9 +41,7 @@ $db_handle = new DBController();
   <link rel="stylesheet" href="assets/theme/css/style.css">
   <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
   <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
   
   
   
